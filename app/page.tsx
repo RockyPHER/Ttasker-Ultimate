14dce5cada88cd6bd0230dfd1a215f0127faafa5
RockyPHER <davidsonjunior10@gmail.com>
2023-11-30 01:13:45 -0300

    Added: icons,  TODO: add timer interactivity

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Timer from '@/components/Timer/TimerMain'
 
 export default function Home() {
   return (
-    <main className='w-screen h-screen translate-y-[-16vh] flex flex-col items-center justify-center'>
+    <main className='w-screen h-screen translate-y-[-16vh] bg-[--background-end-rgb] flex flex-col items-center justify-center'>
       <Timer />
     </main>
   );
--- app/ui/Timer/TimerMain.tsx
+++ app/ui/Timer/TimerMain.tsx
@@ -2,6 +2,11 @@
 
 import React from "react"
 import { useState, useEffect } from "react"
+import PlayIcon from "@/icons/play.svg"
+
+interface timerButtonProps {
+    onClickHandler: () => void
+}
 
 export default function Timer() {
 
@@ -9,20 +14,41 @@ export default function Timer() {
 
     const [minutes, seconds] = mock;
     
+    function timer (time: number) {
+
+    }
+
+    function startTimer () {
+
+    }
+
+    function playButtonHandler () {
+    }
+
+    function convertTimeMsToString (time: number) {
+    }
+    function convertTimeStringToMs (minutes: string, seconds: string) {
+    }
+
     return(
-        <div className="select-none w-auto h-[26vh] px-4 flex items-center justify-center bg-gray-600 bg-opacity-50 ">
-            <span className="text-[25vh]">{minutes}</span>
-            <span className="text-[25vh]">:</span>
-            <span className="text-[25vh]">{seconds}</span>
+        <div className="flex flex-col space-y-5">
+            <div className="select-none w-auto h-[26vh] px-4 flex items-center justify-center bg-gray-600 bg-opacity-50 ">
+                <span className="text-[25vh]">{minutes}</span>
+                <span className="text-[25vh]">:</span>
+                <span className="text-[25vh]">{seconds}</span>
+            </div>
+            <div>
+                <TimerButton onClickHandler={playButtonHandler} />
+            </div>
         </div>
     );
 }
 
-export function TimerButton () {
+export function TimerButton ({onClickHandler}: timerButtonProps) {
     return (
-        <div>
-            <button>
-                <PlayIcon className="w-6 h-6"/>
+        <div className="flex justify-center items-center w-full">
+            <button onClick={onClickHandler} className="flex items-center justify-center py-1 px-6 bg-gray-500 bg-opacity-50 hover:bg-gray-700 hover:bg-opacity-50 active:bg-black active:bg-opacity-50">
+                <PlayIcon className="w-16 h-16"/>
             </button>
         </div>
     );
